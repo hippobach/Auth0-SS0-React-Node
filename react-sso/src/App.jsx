dc088cdcd576201f7272cef6d8afab88bba6092a
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-09-28 00:12:07 +0700

    fix name tag for app

--- react-sso/src/App.jsx
+++ react-sso/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-import Dashboard from './pages/Dashboard'
-import LoginButton from './components/LoginButton'
-import LogoutButton from './components/LogoutButton'
+import './App.css';
+import Dashboard from './pages/Dashboard';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
     <div className="app-container">
       <div className="fixed-box">
-        <h1>Auth0 SSO | TrungQuanDev</h1>
+        <h1>Auth0 SSO | Bach Nguyen</h1>
         <div className="actions">
           {/* 2 Button Login và Logout tùy điều kiện hiển thị */}
           <LoginButton />
@@ -15,9 +15,9 @@ function App() {
         </div>
         {/* Phần dashboard sau khi đăng nhập */}
         <Dashboard />
-      </div>      
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
