4ab9d77fd7370b23edfe5f94bb6e29c142af2e24
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-10-01 00:08:32 +0700

    integrate universal login with Auth0

--- react-sso/src/App.jsx
+++ react-sso/src/App.jsx
@@ -1,17 +1,18 @@
+import { useAuth0 } from '@auth0/auth0-react';
+
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="app-container">
       <div className="fixed-box">
         <h1>Auth0 SSO | Bach Nguyen</h1>
         <div className="actions">
-          {/* 2 Button Login và Logout tùy điều kiện hiển thị */}
-          <LoginButton />
-          <LogoutButton />
+          {!isAuthenticated ? <LoginButton /> : <LogoutButton />}
         </div>
         {/* Phần dashboard sau khi đăng nhập */}
         <Dashboard />
--- react-sso/src/pages/Dashboard.jsx
+++ react-sso/src/pages/Dashboard.jsx
@@ -1,51 +1,50 @@
 import ReactJson from 'react-json-view';
+import { useAuth0 } from '@auth0/auth0-react';
 
 const Dashboard = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  console.log(user);
+
+  if (!isAuthenticated) return null;
+
   return (
     <div className="dashboard">
       <div className="user-from-auth0">
         <div className="title">User from Auth0:</div>
         <div className="preview-user">
-          {/* <div className="loading">Loading...</div> */}
-          <img
-            className="user-avatar"
-            src={
-              'https://scontent.fhan19-1.fna.fbcdn.net/v/t39.30808-6/447709831_2142700316096950_7950542593462786610_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFO508ltptf6k63LzoURpmBMuFtQN4uAPMy4W1A3i4A8yE-aaGhzw-gNDjCge-c6XTm3NR61q2UMrXym5kLHQ9U&_nc_ohc=hjNv-91PJCIQ7kNvgEaQthz&_nc_ht=scontent.fhan19-1.fna&_nc_gid=A0QlGOUq39I0s-sU2uxwYO7&oh=00_AYDRG6htw8cKkXfVr9Pm83g91dXJmpOhAAvRnw8Xf3p_JQ&oe=66FCAF80'
-            }
-            alt={'Bach Nguyen'}
-          />
-          <div className="user-info">
-            <p>
-              Sub:{' '}
-              <span className="value">google-oauth2|108184243235574894333</span>
-            </p>
-            <p>
-              Email: <span className="value">bachnguyen@gmail.com</span>
-            </p>
-            <p>
-              Name: <span className="value">Bach Nguyen</span>
-            </p>
+          {isLoading ? (
+            <div className="loading">Loading...</div>
+          ) : (
+            <>
+              <img
+                className="user-avatar"
+                src={user?.picture}
+                alt={user?.name}
+              />
+              <div className="user-info">
+                <p>
+                  Sub: <span className="value">{user?.sub}</span>
+                </p>
+                <p>
+                  Email: <span className="value">{user?.email}</span>
+                </p>
+                <p>
+                  Name: <span className="value">{user?.name}</span>
+                </p>
+              </div>
+            </>
+          )}
+        </div>
+        {user && (
+          <div className="more-info">
+            <ReactJson
+              enableClipboard={false}
+              collapsed={true}
+              theme={'google'}
+              src={user}
+            />
           </div>
-        </div>
-        <div className="more-info">
-          <ReactJson
-            enableClipboard={false}
-            collapsed={true}
-            theme={'google'}
-            src={{
-              given_name: 'Ragdoll',
-              family_name: 'Cats',
-              nickname: 'bachnguyen2',
-              name: 'Bach Nguyen',
-              picture:
-                'https://scontent.fhan19-1.fna.fbcdn.net/v/t39.30808-6/447709831_2142700316096950_7950542593462786610_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeFO508ltptf6k63LzoURpmBMuFtQN4uAPMy4W1A3i4A8yE-aaGhzw-gNDjCge-c6XTm3NR61q2UMrXym5kLHQ9U&_nc_ohc=hjNv-91PJCIQ7kNvgEaQthz&_nc_ht=scontent.fhan19-1.fna&_nc_gid=A0QlGOUq39I0s-sU2uxwYO7&oh=00_AYDRG6htw8cKkXfVr9Pm83g91dXJmpOhAAvRnw8Xf3p_JQ&oe=66FCAF80',
-              updated_at: '2024-07-10T09:59:37.603Z',
-              email: 'bachnguyen2@gmail.com',
-              email_verified: true,
-              sub: 'google-oauth2|108184243235574894333',
-            }}
-          />
-        </div>
+        )}
       </div>
 
       <div className="user-from-our-database">
